530aeeb3be82ace991bbdde9b87995ef7c2ed360
mansuur-iman <imaninspires1@gmail.com>
2026-04-23 16:01:14 +0300

    added login api in authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -15,3 +15,18 @@ export async function register(data) {
 
   return responseData;
 }
+
+export async function userLogin(data) {
+  const res = await fetch("https://myapp.com/api/v1/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  const responseData = res.json();
+
+  if (!res.ok) throw new Error(responseData.msg || "Login failed.");
+  return responseData;
+}
